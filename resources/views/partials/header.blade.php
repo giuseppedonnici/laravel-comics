b242f14b1c5fbca660d6d091ee0b249b458c5708
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-05-24 10:40:10 +0200

    Add: forEach for the menu header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,48 @@
+@php
+    $menu = [
+        [
+            'title' => 'characters',
+            'link' => 'characters'
+        ],
+        [
+            'title' => 'comics',
+            'link' => 'comics'
+        ],
+        [
+            'title' => 'movies',
+            'link' => 'movies'
+        ],
+        [
+            'title' => 'tv',
+            'link' => 'home'
+        ],
+        [
+            'title' => 'games',
+            'link' => 'home'
+        ],
+        [
+            'title' => 'collectibles',
+            'link' => 'home'
+        ],
+        [
+            'title' => 'videos',
+            'link' => 'home'
+        ],
+        [
+            'title' => 'fans',
+            'link' => 'home'
+        ],
+        [
+            'title' => 'news',
+            'link' => 'home'
+        ],
+        [
+            'title' => 'shop',
+            'link' => 'home'
+        ],
+    ];
+@endphp
+
 <header>
     <div class="container my-4">
         <nav class="nav-bar d-flex justify-content-between align-items-center">
@@ -5,36 +50,11 @@
                 <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="logo image">
             </a>
             <ul class="w-100 list-unstyled d-flex justify-content-around">
+                @foreach ($menu as $item)
                 <li class="text-uppercase">
-                    <a href="{{url('/characters')}}">characters</a>
+                    <a href="{{route( $item['link']) }}">{{ $item['title'] }}</a>
                 </li>
-                <li class="text-uppercase">
-                    <a href="{{url('/comics')}}">comics</a>
-                </li>
-                <li class="text-uppercase">
-                    <a href="{{url('/movies')}}">movies</a>
-                </li>
-                <li class="text-uppercase">
-                    <a href="">tv</a>
-                </li>
-                <li class="text-uppercase">
-                    <a href="">games</a>
-                </li>
-                <li class="text-uppercase">
-                    <a href="">collectibles</a>
-                </li>
-                <li class="text-uppercase">
-                    <a href="">videos</a>
-                </li>
-                <li class="text-uppercase">
-                    <a href="">fans</a>
-                </li>
-                <li class="text-uppercase">
-                    <a href="">news</a>
-                </li>
-                <li class="text-uppercase">
-                    <a href="">shop</a>
-                </li>
+                @endforeach
             </ul>
         </nav>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $comics = config('comics');
     return view('home', compact('comics'));
-});
+})->name('home');
 
 Route::get('/characters', function() {
     return view(('characters'));
-});
+})->name('characters');
 
 Route::get('/comics', function() {
     return view(('comics'));
-});
+})->name('comics');
 
 Route::get('/movies', function() {
     return view(('movies'));
-});
+})->name('movies');
